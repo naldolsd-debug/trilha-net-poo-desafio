9fb0dc4cd6e4aa404e243ed5a6896c2faa0abd10
naldolsd-debug <naldo.lsd@gmail.com>
2025-11-18 06:49:26 -0300

    Classes

---
+++ Smartphone.cs
@@ -0,0 +1,30 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public float Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, float memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
